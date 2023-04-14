03ac3728974706332e5beb42beee812f192d1b88
Ray <105521700+Barrow75@users.noreply.github.com>
2023-04-14 02:11:25 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# A simple calculator that takes just the simple arithmatic (Add, Subtract, Multiple and Divide)
+
+
+def Calculator(Number_1, Number_2, operator):
+    if operator == "+":
+        return Number_1 + Number_2
+    elif operator == "-":
+        return Number_1 - Number_2
+
+    elif operator == "*":
+        return Number_1 * Number_2
+
+    elif operator == "/":
+
+        return Number_1 / Number_2
+
+    else:
+        return "Invalid"
+
+
+Number_1 = float(input("Enter First Number: "))
+operator = input("Enter Operator: ")
+Number_2 = float(input("Enter Second Number: "))
+result = Calculator(Number_1, Number_2, operator)
+print(result)
+
